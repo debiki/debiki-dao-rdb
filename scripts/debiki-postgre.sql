a34047d2ab88759ea805dd5629ae1031b7e48a14
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-04-16 18:51:50 +0200

    Update table definitions, remove todo notes for things done.

--- scripts/debiki-postgre.sql
+++ scripts/debiki-postgre.sql
@@ -455,33 +455,9 @@ create sequence DW1_PAGES_SNO start with 10;
 -- Contains all posts, edits, ratings etc, everything that's needed to
 -- render a discussion.
 create table DW1_PAGE_ACTIONS(   -- abbreviated PGAS (PACTIONS deprectd abbrv.)
-  PAGE varchar(32)     not null, -- should remove..
-  --------------------
-  ----- todo: dev,prod, done@test:  alter table DW1_PAGE_ACTIONS add
-  -- TENANT varchar(32)  not null,  -- ..use instead...
-  -- PAGE_ID varchar(32)  not null,  -- ... of PAGE
-  -- update DW1_PAGE_ACTIONS set
-  --   PAGE_ID = (select p.GUID from DW1_PAGES p where p.SNO = PAGE)
-  --   TENANT = (select p.TENANT from DW1_PAGES p where p.SNO = PAGE)
-  -- alter table DW1_PAGE_ACTIONS alter column TENANT set not null;
-  -- alter table DW1_PAGE_ACTIONS alter column PAGE_ID set not null;
-  -- alter table DW1_PAGE_ACTIONS alter column PAGE drop not null;
-  --
-  -- drop constraint DW1_PACTIONS__R__PACTIONS;
-  -- drop constraint DW1_PACTIONS_PAGE_PAID__P;
-  -- add constraint DW1_PGAS_TNT_PGID_ID__P
-  --   primary key (TENANT, PAGE_ID, PAID),
-  -- add constraint DW1_PGAS__R__PGAS
-  --  foreign key (TENANT, PAGE_ID, RELPA) -- no ix: no dels/upds in prnt tbl
-  --                     -- and no joins (loading whole page at once instead)
-  --  references DW1_PAGE_ACTIONS (TENANT, PAGE_ID, PAID) deferrable,
-  -- add constraint DW1_PGAS_TNT_PGID__R__PGPTHS
-  --  foreign key (TENANT, PAGE_ID)
-  --  references DW1_PAGE_PATHS (TENANT, PAGE_ID) deferrable,
-  ----- and when everything ok:
-  -- alter table DW1_PAGE_ACTIONS drop column PAGE;
-  --
-  --------------------
+  PAGE varchar(32), -- deprecated, no longer written to
+  TENANT varchar(32)  not null,
+  PAGE_ID varchar(32)  not null,
   PAID varchar(32)     not null,  -- page action id  COULD rename to ID
   LOGIN varchar(32)    not null,  -- COULD rename to LOGIN_ID
   TIME timestamp       not null,  -- COULD rename to CTIME
@@ -510,17 +486,21 @@ create table DW1_PAGE_ACTIONS(   -- abbreviated PGAS (PACTIONS deprectd abbrv.)
   MARKUP varchar(30),
   WHEERE varchar(150),
   NEW_IP varchar(39), -- null, unless differs from DW1_LOGINS.START_IP
-  constraint DW1_PACTIONS_PAGE_PAID__P primary key (PAGE, PAID),
+ alter table DW1_PAGE_ACTIONS add constraint DW1_PGAS_TNT_PGID_ID__P
+     primary key (TENANT, PAGE_ID, PAID);
+ alter table DW1_PAGE_ACTIONS add constraint DW1_PGAS_TNT_PGID__R__PGPTHS
+     foreign key (TENANT, PAGE_ID)
+     references DW1_PAGE_PATHS (TENANT, PAGE_ID) deferrable;
+ alter table DW1_PAGE_ACTIONS add constraint DW1_PGAS__R__PGAS
+     foreign key (TENANT, PAGE_ID, RELPA) -- no ix! no dels/upds in prnt tbl
+                       -- and no joins (loading whole page at once instead)
+     references DW1_PAGE_ACTIONS (TENANT, PAGE_ID, PAID) deferrable;
   constraint DW1_PACTIONS__R__LOGINS  -- ix DW1_PACTIONS_LOGIN
       foreign key (LOGIN)
       references DW1_LOGINS (SNO) deferrable,
-  constraint DW1_PACTIONS__R__PAGES  -- ix DW1_PACTIONS_PAGE_PAID__P
+  constraint DW1_PACTIONS__R__PAGES -- deprecated. Ix DW1_PACTIONS_PAGE_PAID__P
       foreign key (PAGE)
       references DW1_PAGES (SNO) deferrable,
-  constraint DW1_PACTIONS__R__PACTIONS
-      foreign key (PAGE, RELPA) -- no index: no deletes/upds in parent table
-                         -- and no joins (loading whole page at once instead)
-      references DW1_PAGE_ACTIONS (PAGE, PAID) deferrable,
   -- The body and title and template etc. have ids 1, 2, 3 etc, i.e. one
   -- char only, and are their own parents.
   -- No other actions may have 1 char ids.
@@ -608,27 +588,12 @@ create index DW1_PACTIONS_LOGIN on DW1_PAGE_ACTIONS(LOGIN);
 
 -- COULD rename to DW1_ACTION_RATINGS.
 create table DW1_PAGE_RATINGS(  -- abbreviated ARTS? PRATINGS deprctd.
-  --- todo prod,dev done@test:
-  -- alter table DW1_PAGE_RATINGS add column
   TENANT varchar(32) not null,
   PAGE_ID varchar(32) not null,
-  -- update DW1_PAGE_RATINGS set
-  --    TENANT = (select a.TENANT from DW1_PAGE_ACTIONS a where a.PAGE = PAGE
-  --   and a.PAID = PAID limit 1),
-  --    PAGE_ID = (select a.PAGE_ID from DW1_PAGE_ACTIONS a where a.PAGE = PAGE and a.PAID = PAID limit 1);
-  -----
-  PAGE varchar(32) not null, -- COULD rename to PAGE_ID
+  PAGE varchar(32), -- deprecated, no longer written to
   PAID varchar(32),
   TAG varchar(30) not null,
   -- (will be dropped: done@test)
-  constraint DW1_PRATINGS__P primary key (PAGE, PAID, TAG),
-  constraint DW1_PRATINGS__R__PACTIONS  -- ix DW1_PRATINGS__P
-      foreign key (PAGE, PAID)
-      references DW1_PAGE_ACTIONS(PAGE, PAID) deferrable
-  --- todo prod,dev, done@test:
-  -- alter table DW1_PAGE_RATINGS drop constraint DW1_PRATINGS__P;
-  -- alter table DW1_PAGE_RATINGS drop constraint DW1_PRATINGS__R__PACTIONS;
-  -- alter table DW1_PAGE_RATINGS add
   constraint DW1_ARTS__P primary key (TENANT, PAGE_ID, PAID, TAG),
   constraint DW1_ARTS__R__PGAS  -- ix primary key
       foreign key (TENANT, PAGE_ID, PAID)
@@ -848,81 +813,3 @@ where SHOW_ID = 'F';
 -- shown before the page slug).
 create index DW1_PGPTHS_ALL
 on DW1_PAGE_PATHS(TENANT, PARENT_FOLDER, PAGE_SLUG, PAGE_ID);
-
-/* (done: prod, dev, test)
-insert into DW1_PAGE_PATHS (
-  TENANT, PARENT_FOLDER, PAGE_ID,
-  PAGE_SLUG,
-  SHOW_ID,
-  PAGE_STATUS, CACHED_TITLE, CACHED_PUBL_TIME, CACHED_SGFNT_MTIME)
-select
-  TENANT, FOLDER, PAGE_GUID,
-  case PAGE_NAME when ' ' then '-' else PAGE_NAME end,
-  GUID_IN_PATH,
-  'D', null, null, null
-from DW1_PATHS;
-*/
-
-
-/*  Splits DW1_PAGE_ACTIONS.PAGE into TENANT and PAGE_ID:
-------------------------------------------
-
----
-alter table DW1_PAGE_ACTIONS add TENANT varchar(32);
-
-alter table DW1_PAGE_ACTIONS add PAGE_ID varchar(32);
-
-update DW1_PAGE_ACTIONS a set
-  PAGE_ID = (select p.GUID from DW1_PAGES p where p.SNO = a.PAGE),
-  TENANT = (select p.TENANT from DW1_PAGES p where p.SNO = a.PAGE);
-
-alter table DW1_PAGE_ACTIONS alter column TENANT set not null;
-alter table DW1_PAGE_ACTIONS alter column PAGE_ID set not null;
-
----
-alter table DW1_PAGE_RATINGS add column TENANT varchar(32);
-alter table DW1_PAGE_RATINGS add column PAGE_ID varchar(32);
---
-
-update DW1_PAGE_RATINGS r set
-   TENANT = (select a.TENANT from DW1_PAGE_ACTIONS a where a.PAGE = r.PAGE and a.PAID = r.PAID),
-   PAGE_ID = (select a.PAGE_ID from DW1_PAGE_ACTIONS a where a.PAGE = r.PAGE and a.PAID = r.PAID);
-
-select * from DW1_PAGE_RATINGS limit 200;
-
-alter table DW1_PAGE_RATINGS drop constraint DW1_PRATINGS__P;
-alter table DW1_PAGE_RATINGS drop constraint DW1_PRATINGS__R__PACTIONS;
-alter table DW1_PAGE_RATINGS add constraint DW1_ARTS__P primary key (TENANT, PAGE_ID, PAID, TAG);
-
----
- alter table DW1_PAGE_ACTIONS drop constraint DW1_PACTIONS__R__PACTIONS;
- alter table DW1_PAGE_ACTIONS drop constraint DW1_PACTIONS_PAGE_PAID__P;
- alter table DW1_PAGE_ACTIONS add constraint DW1_PGAS_TNT_PGID_ID__P
-   primary key (TENANT, PAGE_ID, PAID);
- alter table DW1_PAGE_ACTIONS add constraint DW1_PGAS__R__PGAS
-    foreign key (TENANT, PAGE_ID, RELPA) -- no ix: no dels/upds in prnt tbl
-                       -- and no joins (loading whole page at once instead)
-    references DW1_PAGE_ACTIONS (TENANT, PAGE_ID, PAID) deferrable;
- alter table DW1_PAGE_ACTIONS add constraint DW1_PGAS_TNT_PGID__R__PGPTHS
-  foreign key (TENANT, PAGE_ID)
-  references DW1_PAGE_PATHS (TENANT, PAGE_ID) deferrable;
-
----
-alter table DW1_PAGE_RATINGS add
-  constraint DW1_ARTS__R__PGAS  -- ix primary key
-      foreign key (TENANT, PAGE_ID, PAID)
-      references DW1_PAGE_ACTIONS(TENANT, PAGE_ID, PAID) deferrable;
-
-alter table DW1_PAGE_RATINGS alter column TENANT set not null;
-alter table DW1_PAGE_RATINGS alter column PAGE_ID set not null;
-
----
-alter table DW1_PAGE_ACTIONS alter column PAGE drop not null;
-alter table DW1_PAGE_RATINGS alter column PAGE drop not null;
-
--------------------------------------
------------- done above, @dev, @test, @prod.
-
-
-*/
-
