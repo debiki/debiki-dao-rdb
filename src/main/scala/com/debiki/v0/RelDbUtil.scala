ef49e6f7da66196202507399fc24e011b93d389d
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-06-21 07:41:49 +0200

    Check for Approval.Temporary.

--- src/main/scala/com/debiki/v0/RelDbUtil.scala
+++ src/main/scala/com/debiki/v0/RelDbUtil.scala
@@ -300,6 +300,8 @@ object RelDbUtil {
 
   def _toDbVal(approval: Option[Approval]): AnyRef = approval match {
     case None => NullVarchar
+    // Temporary approvals are for one HTTP request only and must not be saved.
+    case Some(Approval.Temporary) => assErr("DwE25Kf0", "Got an Approval.Temporary")
     case Some(Approval.Preliminary) => "P"
     case Some(Approval.WellBehavedUser) => "W"
     case Some(Approval.AuthoritativeUser) => "A"
