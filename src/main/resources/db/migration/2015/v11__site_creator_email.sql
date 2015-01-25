88d03a7e553fc5bb5c45698f00ea2c6596c58309
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-01-25 04:40:40 +0100

    Set DW1_TENANTS.NAME to not-null.

--- src/main/resources/db/migration/2015/v11__site_creator_email.sql
+++ src/main/resources/db/migration/2015/v11__site_creator_email.sql
@@ -7,8 +7,10 @@ alter table dw1_tenants add constraint dw1_tnt_creatoremail
     check (creator_email_address like '%@%.%');
 
 update dw1_tenants set creator_email_address = 'unknown@example.com';
+update dw1_tenants set name = 'embedded-site-' || id where name is null;
 
 alter table dw1_tenants alter creator_email_address set not null;
+alter table dw1_tenants alter name set not null;
 
 create index dw1_tenants_creatoremail on dw1_tenants(creator_email_address);
 
