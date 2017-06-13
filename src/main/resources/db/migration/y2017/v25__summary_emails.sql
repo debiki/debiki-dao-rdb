7e8ddb5e19cc67d833d7945b0d8d71e718aee2ca
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2017-06-13 07:17:51 +0300

    Add summary email settings.

---
+++ src/main/resources/db/migration/y2017/v25__summary_emails.sql
@@ -0,0 +1,6 @@
+
+alter table users3 add column summary_email_interval_mins int;
+alter table users3 add column summary_email_if_active bool;
+
+alter table settings3 add column summary_email_sync_date timestamp;
+
