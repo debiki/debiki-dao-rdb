d9e0f3ee3c0a813c86aba155c8a96ea7cf6cdfd5
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2018-08-28 16:58:26 +0200

    Code review.

--- src/main/resources/db/migration/y2018/v371__drafts.sql
+++ src/main/resources/db/migration/y2018/v371__drafts.sql
@@ -8,19 +8,19 @@ create table drafts3 (
   last_edited_at timestamp,
   deleted_at timestamp,
   category_id int,
+  to_user_id int,
   topic_type smallint,
   page_id varchar,
   post_nr int,
   post_id int,
   post_type smallint,
-  to_user_id int,
   title varchar not null,
   text varchar not null,
 
-  constraint drafts_nr_p primary key (site_id, by_user_id, draft_nr),
+  constraint drafts_byuser_nr_p primary key (site_id, by_user_id, draft_nr),
 
 	-- ix: pk
-  constraint drafts_author_r_users foreign key (
+  constraint drafts_byuser_r_users foreign key (
       site_id, by_user_id) references users3 (site_id, user_id) deferrable,
 
 	-- ix: drafts_category_i
@@ -31,12 +31,16 @@ create table drafts3 (
   constraint drafts_touser_r_users foreign key (
       site_id, to_user_id) references users3 (site_id, user_id) deferrable,
 
+  -- ix: drafts_pageid_postnr_i (sometimes with null postnr)
+  constraint drafts_pageid_r_pages foreign key (
+      site_id, page_id) references pages3 (site_id, page_id) deferrable,
+
   -- ix: drafts_pageid_postnr_i
-  constraint drafts_posttoreplyto_r_posts foreign key (
+  constraint drafts_pageid_postnr_r_posts foreign key (
       site_id, page_id, post_nr) references posts3 (site_id, page_id, post_nr) deferrable,
 
 	-- ix: drafts_postid_i
-  constraint drafts_posttoedit_r_posts foreign key (
+  constraint drafts_postid_r_posts foreign key (
       site_id, post_id) references posts3 (site_id, unique_post_id) deferrable,
 
   constraint drafts_c_nr_gte_1 check (draft_nr >= 1),
